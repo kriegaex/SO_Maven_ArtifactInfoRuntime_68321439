c8c9ebaf8c7af2d55764469e82cd26a57649feb5
kes7ragi <60322826+kes7ragi@users.noreply.github.com>
2021-07-11 23:38:13 +0530

    Update Application.java
    Added a new function which will be called by other projects by adding its jar

--- application/src/main/java/de/scrum_master/stackoverflow/q68321439/application/Application.java
+++ application/src/main/java/de/scrum_master/stackoverflow/q68321439/application/Application.java
@@ -1,5 +1,14 @@
 package de.scrum_master.stackoverflow.q68321439.application;
 
 public class Application {
-  public static void main(String[] args) {}
+ // public static void main(String[] args) {}
+  
+  //adding common function which will be called by other projects by adding this module jar as the dependency.
+  public void mavenModule(){
+    MavenModuleInfo moduleInfo = getClass().getPackage().getAnnotation(MavenModuleInfo.class);
+    System.out.println(""+moduleInfo.groupId());
+    System.out.println(""+moduleInfo.artifactId());
+    
+  }
+  
 }
