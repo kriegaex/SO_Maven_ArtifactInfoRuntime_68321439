d2566521d4d9b6555e0ba76d04877770a1fe8845
Alexander Kriegisch <Alexander@Kriegisch.name>
2021-07-12 09:28:41 +0700

    Add example code from StackOverflow answer update

--- application/src/main/java/de/scrum_master/stackoverflow/q68321439/application/Application.java
+++ application/src/main/java/de/scrum_master/stackoverflow/q68321439/application/Application.java
@@ -1,5 +1,13 @@
 package de.scrum_master.stackoverflow.q68321439.application;
 
+import de.scrum_master.stackoverflow.q68321439.library.LibraryClass;
+
 public class Application {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    new LibraryClass().doSomething();
+    System.out.println();
+    new LibraryClass().doSomethingJava9();
+    System.out.println();
+    new LibraryClass().doSomethingClassLoader();
+  }
 }
--- library/src/main/java/de/scrum_master/stackoverflow/q68321439/library/LibraryClass.java
+++ library/src/main/java/de/scrum_master/stackoverflow/q68321439/library/LibraryClass.java
@@ -1,3 +1,44 @@
 package de.scrum_master.stackoverflow.q68321439.library;
 
-public class LibraryClass {}
+import de.scrum_master.stackoverflow.q68321439.annotation.MavenModuleInfo;
+
+public class LibraryClass {
+  public void doSomething() {
+    StackTraceElement callerStackTraceElement = new Exception().getStackTrace()[1];
+    try {
+      Class<?> callerClass = Class.forName(callerStackTraceElement.getClassName());
+      MavenModuleInfo mavenModuleInfo = callerClass.getPackage().getAnnotation(MavenModuleInfo.class);
+      System.out.println(mavenModuleInfo.groupId());
+      System.out.println(mavenModuleInfo.artifactId());
+      System.out.println(mavenModuleInfo.version());
+    }
+    catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void doSomethingJava9() {
+    Class<?> callerClass = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).getCallerClass();
+    MavenModuleInfo mavenModuleInfo = callerClass.getPackage().getAnnotation(MavenModuleInfo.class);
+    System.out.println(mavenModuleInfo.groupId());
+    System.out.println(mavenModuleInfo.artifactId());
+    System.out.println(mavenModuleInfo.version());
+  }
+
+  public void doSomethingClassLoader() {
+    StackTraceElement callerStackTraceElement = new Exception().getStackTrace()[1];
+    try {
+      Class<?> callerClass = Class.forName(callerStackTraceElement.getClassName());
+      // Cheap way of getting Maven artifact name - TODO: parse
+      System.out.println(
+        callerClass
+          .getClassLoader()
+          .getResource(callerStackTraceElement.getClassName().replaceAll("[.]", "/") + ".class")
+      );
+    }
+    catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
